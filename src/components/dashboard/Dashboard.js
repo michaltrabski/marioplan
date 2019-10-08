625daa9205cb3cb22d33ebfb461e57b8353b0502
Michal Trabski <michal.trabski@gmail.com>
2019-10-08 08:30:22 +0200

    removed store folder and changed index.js,Dashboard.js

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import ProjectList from "../projects/ProjectList";
 import Notifications from "./Notifications";
-import { connect } from "react-redux";
 
 class Dashboard extends Component {
   render() {
@@ -10,7 +9,7 @@ class Dashboard extends Component {
       <div className="dashboard container">
         <div className="row">
           <div className="col s12 m6">
-            <ProjectList projects={this.props.projects} />
+            <ProjectList />
           </div>
           <div className="col s12 m5 offset-m1">
             <Notifications />
@@ -21,8 +20,4 @@ class Dashboard extends Component {
   }
 }
 
-const mapStateToProps = state => {
-  return { projects: state.project.projects };
-};
-
-export default connect(mapStateToProps)(Dashboard);
+export default Dashboard;
