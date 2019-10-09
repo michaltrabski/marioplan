1d93f69742896f24eb2c7012c16be6a1474a84f4
Michal Trabski <michal.trabski@gmail.com>
2019-10-09 12:10:37 +0200

    added firebaseConfig file

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,20 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyC00NLwblwg7EO5gvLgIdXNlnLZBlbpgZE",
+  authDomain: "marioplan-754fd.firebaseapp.com",
+  databaseURL: "https://marioplan-754fd.firebaseio.com",
+  projectId: "marioplan-754fd",
+  storageBucket: "",
+  messagingSenderId: "644513862023",
+  appId: "1:644513862023:web:445323e20f3efea51f4322",
+  measurementId: "G-3H7RHZT24D"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.firetore().settings({ timestampsInSnapshots: true });
+
+export default firebase;
